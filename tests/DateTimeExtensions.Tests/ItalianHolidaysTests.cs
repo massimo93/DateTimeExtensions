9760a105b13c2428e95da5caed45787e83579a58
Massimo Serra <massimosrr@gmail.com>
2024-08-24 21:23:47 +0200

    Init adding Italian holidays tests class

---
+++ tests/DateTimeExtensions.Tests/ItalianHolidaysTests.cs
@@ -0,0 +1,18 @@
+using DateTimeExtensions.WorkingDays;
+using DateTimeExtensions.WorkingDays.CultureStrategies;
+using NUnit.Framework;
+
+namespace DateTimeExtensions.Tests
+{
+    [TestFixture]
+    public class ItalianHolidaysTests
+    {
+        [Test]
+        public void CanGetHolidayStrategy()
+        {
+            var dateTimeCulture = new WorkingDayCultureInfo("it-IT");
+            var holidaysStrategy = dateTimeCulture.LocateHolidayStrategy(dateTimeCulture.Name, null);
+            Assert.That(holidaysStrategy.GetType(), Is.EqualTo(typeof(IT_ITHolidayStrategy)));
+        }
+    }
+}
